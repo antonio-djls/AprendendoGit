c7c876bdcde861e9fa6b8a13310bd52e6ecf248d
antonio-djls <antoniodinizjorgelimasaraiva@gmail.com>
2025-11-23 00:09:22 -0300

    branch 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    std::cout << "hwll" << std::endl;
+    std::cout << "branch main" << std::endl;
+    return 0;
+}
--- 3.cpp
+++ 3.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 int main(){
+    int x{};
+    int j{};
+    //commit
+    std::cout << "asd" << std::endl;
     std::cout << "Hello world" << std::endl;
     return 0;
+}
+void print(){
+    std::cout << "ho" << std::endl;
+    std::cout << "dsad";
 }
